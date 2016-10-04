e37bb8118eb587d1e5db55a41f9e51d526de5ba1
James Demeuse <james.demeuse@gmail.com>
2016-10-04 18:52:07 -0500

    init

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,21 @@
+﻿var path = require('path');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+    context: path.join(__dirname, 'App'),
+    entry: './main.js',
+    output: {
+        path: path.join(__dirname, 'Built'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+      new WebpackNotifierPlugin()
+    ],
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$/, loader: "url" }
+        ]
+    }
+    
+};
