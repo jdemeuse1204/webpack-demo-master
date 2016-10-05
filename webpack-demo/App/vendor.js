427df2425ac007b10b7e88d5eb5b59365757e685
jdemeuse <james.demeuse@gmail.com>
2016-10-05 11:00:36 -0500

    Kendo Update

--- webpack-demo/App/vendor.js
+++ webpack-demo/App/vendor.js
@@ -1,17 +1,18 @@
 ﻿// jQuery
-require('../Scripts/kendo/2015.1.318/jquery.min');
+require('../Scripts/kendo/2016.3.914/jquery.min');
 
 // PubSub
 require('../Scripts/pubsub.js');
 
 // Kendo UI CSS
-require('../Content/kendo/2015.1.318/kendo.common.min.css');
-require('../Content/kendo/2015.1.318/kendo.default.min.css');
+require('../Content/kendo/2016.3.914/kendo.common.min.css');
+require('../Content/kendo/2016.3.914/kendo.default.min.css');
 
 // Kendo UI
-require('../Scripts/kendo/2015.1.318/kendo.router.min');
-require('../Scripts/kendo/2015.1.318/kendo.view.min');
-require('../Scripts/kendo/2015.1.318/kendo.fx.min');
+require('../Scripts/kendo/2016.3.914/kendo.core.min');
+require('../Scripts/kendo/2016.3.914/kendo.router.min');
+require('../Scripts/kendo/2016.3.914/kendo.view.min');
+require('../Scripts/kendo/2016.3.914/kendo.fx.min');
 
 // Bootstrap
 require('../Content/bootstrap.css');
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,20 +1,46 @@
-﻿var path = require('path');
+﻿var debug = process.env.NODE_ENV !== "PROD"; // cmd: SET NODE_ENV=development && webpack
+var path = require('path');
+var webpack = require('webpack');
 var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
     context: path.join(__dirname, 'App'),
     entry: './main.js',
+    resolve: {
+        extensions: ['', '.js', 'min.js'],
+        alias: {
+            "jquery": path.resolve("./Scripts/kendo/2016.3.914/jquery.min.js")
+        },
+        root: [
+                path.resolve('.'),
+                path.resolve('./Scripts/kendo/2016.3.914/') // the path to the minified scripts
+        ]
+    },
     output: {
         path: path.join(__dirname, 'Built'),
-        filename: '[name].bundle.js'
+        filename: debug ? '[name].bundle.js' : '[name].bundle.min.js'
     },
-    plugins: [
-      new WebpackNotifierPlugin()
+    plugins: debug ? [
+        new WebpackNotifierPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
+    ] : [
+        new WebpackNotifierPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
     ],
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$/, loader: "url" }
+            { test: /\.css$|\.min.css$/, loader: "style!css" },
+            { test: /.(png|woff(2)?|eot|ttf|gif|svg|jpeg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' }
+            //{ test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$/, loader: "url" }
         ]
     }
     
